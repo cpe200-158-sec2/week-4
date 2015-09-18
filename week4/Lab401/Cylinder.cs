8ee5cf7250de6426e919fa2f3f9163718e1b605e
ardinsia <pakpoomruj@gmail.com>
2015-09-18 17:30:53 +0700

    570610592 lab 401

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,50 @@
 
 namespace Lab4
 {
-	public class Cylinder
-	{
-		public Cylinder ()
-		{
-		}
-	}
+    public class Cylinder : Circle
+    {
+        private double _height;
+        public double Height
+        {
+            get { return _height; }
+            set { if (value >= 0) { _height = value; } }
+        }
+        public Cylinder()
+        {
+            Height = 1.0;
+        }
+        public Cylinder(double r, double h)
+        {
+            Radius = r;
+            Height = h;
+        }
+        public Cylinder(double x, double y, double r, double h)
+        {
+            base.setCircle(x, y, r);
+            Height = h;
+        }
+        public Cylinder(Circle c)
+        {
+            base.setCircle(c.X, c.Y, c.Radius);
+            Height = 1.0;
+        }
+        public Cylinder(Cylinder cl)
+        {
+            base.setCircle(cl.X, cl.Y, cl.Radius);
+            Height = cl.Height;
+        }
+        public override double getArea()
+        {
+            return base.getArea() * 2 + 2 * Pi * Radius * Height;
+        }
+        public double getVolume()
+        {
+            return base.getArea() * Height;
+        }
+        public override string ToString()
+        {
+            string s = "[Cylinder: center(" + X + "," + Y + "), radius=" + Radius + ", height=" + Height + ", surface=" + getArea() + ", volume=" + getVolume() + "]";
+            return s;
+        }
+    }
 }
-
