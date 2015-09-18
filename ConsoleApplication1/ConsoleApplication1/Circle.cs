12c3e508c0a6582f7ef96b62febf6aac560ec784
chaipichit <chaipichiti09@hotmail.com>
2015-09-18 17:27:32 +0700

    570610563 lab 401

---
+++ ConsoleApplication1/ConsoleApplication1/Circle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lab4
+{
+	public class Circle
+	{
+        private Point _Center;
+        private double _radius;
+        public double radius
+        {
+            get { return _radius; }
+            set {
+                if (value >= 0)
+                    _radius = value;
+                else
+                    _radius = 1;
+                }
+        }
+        public Point Center
+        {
+            get { return _Center; }
+            set { _Center = value; }
+        }
+        public Circle ()
+		{
+            _Center = new Point();
+            radius = 1;
+		}
+        public Circle (double a)
+        {
+            _Center = new Point();
+            radius = a;
+        }
+        public Circle(double a,double b,double c)
+        {
+            _Center = new Point(a, b);
+            radius = c;
+        }
+        public Circle (Circle a)
+        {
+            _Center = a._Center;
+            radius = a.radius;               
+        }
+        public double getArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+        public override string ToString()
+        {
+            return string.Format("[Circle: center{0}, radius={1}, area = {2}]", _Center, _radius, getArea());
+        }
+
+    }
+}
+
---
+++ ConsoleApplication1/ConsoleApplication1/Cylinder.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Lab4
+{
+	public class Cylinder : Circle
+	{
+        private Circle _cir;
+        private double _hight;
+        public double hight
+        {
+            get { return _hight; }
+            set
+            {
+                if (value > 0)
+                    _hight = value;
+                else
+                    _hight = 1;
+            }
+        }
+        public Circle cir
+        {
+            get { return _cir; }
+            set { cir = value; }
+        }
+		public Cylinder ()
+		{
+          
+            hight = 1;
+		}
+        public Cylinder(double a,double b)
+        {
+           
+            radius = a;
+            hight = b;
+        }
+        public Cylinder(double a , double b , double c , double d )
+        {
+        
+            Center.x =a;
+            Center.y = b;
+            radius = c;
+            hight = d;
+        }
+        public Cylinder(Circle a)
+        {
+           
+            Center.x = a.Center.x;
+            Center.y = a.Center.y;
+            radius = a.radius;
+            hight = 1;
+        }
+        public Cylinder(Cylinder a)
+        {
+
+            Center.x = a.Center.x;
+            Center.y = a.Center.y;
+            radius = a.radius;
+            hight = a.hight;
+        }
+        public double getArea()
+        {
+            return (Math.PI * radius * radius * 2) + (2 * Math.PI * radius * hight);
+        }
+        public double getVolume()
+        {
+            return Math.PI*radius*radius* hight;
+        }
+        public override string ToString()
+        {
+            return string.Format("[Cylinder: center({0}), radius={1}, height={2}, surface={3}, volume={4}]", Center,radius, _hight, getArea(), getVolume());
+        }
+    }
+}
+
