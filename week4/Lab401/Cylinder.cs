7773549d98160f21d78864b54dba04d686249b7e
Watchakon <WatchakonP@users.noreply.github.com>
2015-09-18 14:31:47 +0700

    Add cylinder code

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -4,9 +4,50 @@ namespace Lab4
 {
 	public class Cylinder
 	{
-		public Cylinder ()
-		{
+		Circle circle = new Circle();
+		double height=1.0;
+		double pi=Math.PI;
+		
+		public Cylinder (){
+			height=1.0;
 		}
+		
+		public Cylinder (double r,double h){
+			height=h; circle.setRad(r); 
+		}
+		
+		public Cylinder (double x,double y, double r, double h){
+			height=h; circle.setRad(r); circle.setXYPos(x,y);
+		}
+		
+		public Cylinder (Circle a){
+			circle.setRad(a.Radius); circle.setPoint(a.getPoint());
+		}
+		
+		public Cylinder (Cylinder a){
+			circle.setRad(a.getRad()); circle.setPoint(a.getPoint()); height=a.height;
+		}
+		
+		public double getRad(){
+			return circle.Radius;
+		}
+		
+		public Point getPoint(){
+			return circle.pointPos;
+		}
+		
+		public double getArea(){
+			return (2*pi*getRad()*height) + (2*circle.getArea());
+		}
+		
+		public double getVolume(){
+			return (height*circle.getArea());
+		}
+		
+		public override string ToString(){
+			return "[Cylinder: " + getPoint() + ", radius=" + getRad() + ", height="+ height +
+				",surface=" + getArea() +", volume= "+getVolume();
+        	}
 	}
 }
 
