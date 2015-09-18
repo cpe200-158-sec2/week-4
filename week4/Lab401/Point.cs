424e2741d35df82b851655f71f379be87c485c05
Watchakon <WatchakonP@users.noreply.github.com>
2015-09-18 14:32:53 +0700

    Add Point Code

--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -3,10 +3,38 @@
 namespace Lab4
 {
 	public class Point
-	{
-		public Point ()
-		{
+    	{
+		double x=0,y=0;
+		
+		public double xpos{
+        		get { return x; }
+            		set { x = value; }
+        	}
+		
+		public double ypos{
+            		get { return y; }
+            		set { y = value; }
+        	}
+		
+		public Point(){ //basic form
+			x=0; y=0;
+        	}
+		
+		public Point(Point a){
+			setValue(a.x,a.y);
 		}
+		
+		public Point(double a, double b){
+			setValue(a,b);
+		}
+		
+		public void setValue(double xpos,double ypos){
+			x=xpos; y=ypos;
+        	}
+		
+		public override string ToString(){
+            		return "center(" + x + "," + y + ")";
+        	}
 	}
 }
 
