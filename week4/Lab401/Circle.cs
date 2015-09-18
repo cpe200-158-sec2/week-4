3227824edd515aaa2e6fb9098403b21b3e0d36e2
ieatbaozi <wildh0rses@hotmail.com>
2015-09-18 17:13:57 +0700

    570610611 lab 4-1

--- week4/Lab401/Circle.cs
+++ week4/Lab401/Circle.cs
@@ -2,11 +2,56 @@
 
 namespace Lab4
 {
-	public class Circle
-	{
-		public Circle ()
-		{
-		}
-	}
+	public class Circle : Point
+    {
+		private double radius;
+        public double Radius
+        {
+            get { return radius; }
+            set
+            {
+                if (value >= 0) radius = value;
+                else radius = 1.0;
+            }
+        }
+
+        public Circle()
+        {
+            setCircle(0, 0, 1.0);
+        }
+        public Circle(double r)
+        {
+            setCircle(0, 0, r);
+        }
+        public Circle(double x, double y, double r)
+        {
+            setCircle(x, y, r);
+        }
+        public Circle(Circle a)
+        {
+            this.X = a.X;
+            this.Y = a.Y;
+            this.Radius = a.Radius;
+        }
+        public void setCircle(double x, double y, double r)
+        {
+            Radius = r;
+            X = x;
+            Y = y;
+        }
+
+        public virtual double getArea()
+        {
+            return Radius * Radius * Math.PI;
+        }
+
+        public override string ToString()
+        {
+            string answer = "[Circle: center(" + X + "," + Y + "), radius=" + Radius + ", area=" + getArea() + "]";
+            return answer;
+        }
+
+    }
 }
+
 
--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,57 @@
 
 namespace Lab4
 {
-	public class Cylinder
-	{
-		public Cylinder ()
-		{
-		}
-	}
+	public class Cylinder : Circle
+    {
+        private double height;
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value >= 0) height = value;
+                else height = 1.0;
+            }
+        }
+        public Cylinder()
+        {
+            Height = 1.0;
+        }
+        public Cylinder(double r, double h)
+        {
+            Height = h;
+            Radius = r;
+        }
+        public Cylinder(double x, double y, double r, double h)
+        {
+            Height = h;
+            base.setCircle(x, y, r);
+        }
+        public Cylinder(Circle a)
+        {
+            Height = 1.0;
+            base.setCircle(a.X, a.Y, a.Radius);
+        }
+        public Cylinder(Cylinder A)
+        {
+            Height = A.Height;
+            base.setCircle(A.X, A.Y, A.Radius);
+        }
+
+        public override double getArea()
+        {
+            return base.getArea() * 2 + 2 * Math.PI * Radius * Height;
+        }
+        public double getVolume()
+        {
+            return base.getArea() * Height;
+        }
+
+        public override string ToString()
+        {
+            string answer = "[Cylinder: center(" + X + "," + Y + "), radius=" + Radius + ", height=" + Height + ", surface=" + getArea() + ", volume=" + getVolume() + "]";
+            return answer;
+        }
+    }
 }
 
