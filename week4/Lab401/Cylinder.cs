64291e36b2348c221296010af338758fc0300344
lekapinun <lekapinun@gmail.com>
2015-09-18 17:28:27 +0700

    570610618 Lab401

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,78 @@
 
 namespace Lab4
 {
-	public class Cylinder
+	public class Cylinder : Circle
 	{
-		public Cylinder ()
+        Circle c = new Circle();
+        //Properties
+        private double _Height;
+
+        public double Height
+        {
+            get { return _Height; }
+            set
+            {
+                if (value < 0)
+                {
+                    _Height = 1;
+                }
+                else
+                {
+                    _Height = value;
+                }
+            }
+        }
+
+        //Constructors
+        public Cylinder ()
 		{
-		}
-	}
+            this.Height = 1;
+        }
+
+        public Cylinder (double r, double h)
+        {
+            this.c = new Circle(r);
+            this.Height = h;
+        }
+
+        public Cylinder (double x, double y, double r, double h)
+        {
+            this.c = new Circle(x,y,r);
+            this.Height = h;
+        }
+
+        public Cylinder (Circle circle)
+        {
+            this.c = new Circle(circle);
+            this.Height = 1;
+        }
+
+        public Cylinder(Cylinder cylinder)
+        {
+            this.c = new Circle(cylinder.c);
+            this.Height = cylinder.Height;
+        }
+
+        //Methods
+        public override double getArea()
+        {
+            double area = 0;
+            area = (2* Math.PI * this.c.Radius * this.c.Radius) + (2 * Math.PI * this.c.Radius * this.Height) ;
+            return area;
+        }
+
+        public double getVolume()
+        {
+            double volume = 0;
+            volume = (Math.PI * this.c.Radius * this.c.Radius * this.Height) ;
+            return volume;
+        }
+
+        public override string ToString()
+        {
+            string s = "[Cylinder: center(" + this.c.Center.x + "," + this.c.Center.y + "), raduis=" + this.c.Radius + ", height=" + this.Height + ", surface =" + this.getArea() + ", volume =" + this.getVolume() + "]";
+            return s;
+        }
+    }
 }
 
