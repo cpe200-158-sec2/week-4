677098f3504ed9eff929a04dcaba248af55f6d62
Channnnnn <osward91@gmail.com>
2015-09-18 17:45:29 +0700

    570610564 Lab402

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -4,9 +4,14 @@ namespace Lab4
 {
 	public abstract class Shape
 	{
-		public Shape ()
+        protected string _color;
+		public Shape (string c = "gold")
 		{
+            _color = c;
 		}
-	}
+        public string Color { get { return _color; } }
+        public abstract double getArea();
+        public abstract double getPerimeter();
+    }
 }
 
