32df77bd7cdb0021ff04abfc209e0f36eb9e4d10
J4Nss <Jelly.barbie.jan@gmail.com>
2015-09-18 17:37:20 +0700

    570610609 Lab401

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,65 @@
 
 namespace Lab4
 {
-	public class Cylinder
+	public class Cylinder : Circle
 	{
+        private double _height;
+        private Circle _circle;
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if (_height <= 0)
+                {
+                    _height = 1.0;
+                }
+                else
+                {
+                    _height = value;
+                }
+            }
+        }
 		public Cylinder ()
 		{
+            _height = 1.0;
+            _circle = new Circle();
 		}
-	}
+        public Cylinder (double r,double h)
+        {
+            _circle.Radius = r;
+            _height = h;
+        }
+        public Cylinder (double a, double b, double r, double h)
+        {
+            _height = h;
+            _circle = new Circle(a, b, r);
+        }
+        public Cylinder (Circle c)
+        {
+            _height = 1.0;
+            _circle = new Circle(c);
+        }
+        public Cylinder (Cylinder a)
+        {
+            _height = a.Height;
+            _circle = new Circle(a._circle.Center.X, a._circle.Center.Y, a._circle.Radius);
+        }
+        public override double getArea()
+        {
+            return (2 * _circle.getArea()) + (Height * 2 * Math.PI * _circle.Radius);
+        }
+        public  double getVolume()
+        {
+            return _circle.getArea() * Height;
+        }
+        public override string ToString()
+        {
+            return string.Format("[Cylinder: center{0}, radius={1}, height={2}, surface={3}, volume={4}]",_circle.Center,_circle.Radius,Height,getArea(),getVolume());
+        }
+    }
 }
 
--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -4,9 +4,48 @@ namespace Lab4
 {
 	public class Point
 	{
+        private double _x, _y;
+        public double X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+        public double Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
 		public Point ()
 		{
+            _x = 0.0;
+            _y = 0.0;
 		}
-	}
+        public Point (Point a)
+        {
+            _x = a.X;
+            _y = a.Y;
+        }
+        public Point (double a, double b)
+        {
+            _x = a;
+            _y = b;
+        }
+        public override string ToString()
+        {
+            return string.Format("({0},{1})",X,Y);
+        }
+    }
 }
 
