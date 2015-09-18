6d6d7636e57f82e70563d877008d62128c86ff77
Peeranut Pongpakatien <peeranut.31@hotmail.com>
2015-09-18 15:41:26 +0700

    570610589 Lab 401

--- week4/Lab401/Circle.cs
+++ week4/Lab401/Circle.cs
@@ -4,9 +4,57 @@ namespace Lab4
 {
 	public class Circle
 	{
-		public Circle ()
+        private double Radius;
+
+        public double radius
+        {
+            get { return Radius; }
+            set { if (value >= 0) Radius = value;
+                else Radius = 1;
+            }                       
+        }
+
+        public Point C =new Point();
+
+        public Circle ()
 		{
+            setCircle(0, 0, 1);     
 		}
-	}
+
+        public Circle(Circle a)
+        {
+            setCircle(a.C.x, a.C.y, a.radius);
+        }
+
+        public Circle(double r)
+        {
+            setCircle(0, 0, r);
+        }
+
+        public Circle(double a , double b , double r)
+        {
+            setCircle(a, b, r);       
+        }
+
+        public virtual void setCircle(double a , double b , double r)
+        {
+            C.x = a;
+            C.y = b;
+            radius = r;
+        }
+
+        public virtual double getArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[Circle: center{0}, radius={1}, area={2}]", C, radius, getArea());
+        }
+
+
+
+    }
 }
 
