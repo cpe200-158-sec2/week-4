c8f8356cb532532744312345ca8c4418d018bc1e
mindmie <dreamiing.allday@gmail.com>
2015-09-18 17:35:35 +0700

    570610612 Lab401

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,81 @@
 
 namespace Lab4
 {
-	public class Cylinder
+	public class Cylinder : Circle //inherit frome circle class
 	{
-		public Cylinder ()
+        //member
+        public Circle Base = new Circle();
+        private double _height;
+        //properties
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+
+            set
+            {   if( value >= 0)
+                {
+                    _height = value;
+                }
+                else
+                {
+                    _height = 1.0;
+                }
+                
+            }
+        }
+
+        //constructor
+        public Cylinder ()
 		{
+            Height = 1.0;
 		}
-	}
+        public Cylinder (double r, double h)
+        {
+            Base.Radius = r;
+            Height = h;    
+        }
+        public Cylinder (double x,double y, double r ,double h)
+        {
+            Base.Center.X = x;
+            Base.Center.Y = y;
+            Base.Radius = r;
+            Height = h;
+        }
+        public Cylinder (Circle a)
+        {
+            Base.Center.X = a.Center.X;
+            Base.Center.Y = a.Center.Y;
+            Base.Radius = a.Radius;
+            Height = 1.0;
+            
+        }
+        public Cylinder (Cylinder b)
+        {
+            Base.Center.X = b.Base.Center.X;
+            Base.Center.Y = b.Base.Center.Y;
+            Base.Radius = b.Base.Radius;
+            Height = b.Height;
+        }
+        //method
+        public override double getArea ()
+        {
+            double area = (2 * Math.PI * Base.Radius*Base.Radius) + (2 * Math.PI * Base.Radius* Height);
+            return area;
+        }
+        public double getVolume()
+        {
+            double volume = Math.PI * Base.Radius * Base.Radius * Height;
+            return volume;
+        }
+        public override string ToString()
+        {
+            string output = ("[Cylinder: center("+ Base.Center.X +","+ Base.Center.Y + "), radius="+ Base.Radius + ", height="+ Height + ", surface="+ getArea() + ", volume="+ getVolume());
+            return output;
+        }
+
+    }
 }
 
--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -4,9 +4,55 @@ namespace Lab4
 {
 	public class Point
 	{
-		public Point ()
+        //members
+        private double _x;
+        private double _y;
+        //properties x
+        public double X
+        {
+            get
+            {
+                return _x;
+            }
+
+            set
+            {
+                _x = value;
+            }
+        }
+        //properties y
+        public double Y
+        {
+            get
+            {
+                return _y;
+            }
+
+            set
+            {
+                _y = value;
+            }
+        }
+        //constructors
+        public Point ()
 		{
+            _x = 0;
+            _y = 0;
 		}
-	}
+        public Point (Point a)
+        {
+            _x = a._x;
+            _y = a._y;
+        }
+        public Point (double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+        public override string ToString()
+        {
+            return string.Format( X + "," + Y);
+        }
+    }
 }
 
