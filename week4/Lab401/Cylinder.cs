bdfac7cd642026825097a02465b1d6b3759b7c7e
awakenedarm <arm.pacino1@gmail.com>
2015-09-18 17:06:04 +0700

    570610560 LAB 4.1 FINISH

--- week4/Lab401/Cylinder.cs
+++ week4/Lab401/Cylinder.cs
@@ -2,11 +2,60 @@
 
 namespace Lab4
 {
-	public class Cylinder
+	public class Cylinder : Circle
 	{
+        private double _height = 1.0;
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                _height = value;
+            }
+        }
+
 		public Cylinder ()
 		{
+            Center = new Point();
 		}
-	}
+        public Cylinder (double rad, double height)
+        {
+            Radius = rad;
+            _height = height;
+            Center = new Point();
+        }
+        public Cylinder (double x, double y, double rad, double height)
+        {
+            Center = new Point(x, y);
+            Radius = rad;
+            _height = height;
+        }
+        public Cylinder (Circle cir){
+            Center = new Point(cir.Center);
+            Radius = cir.Radius;
+        }
+        public Cylinder (Cylinder a)
+        {
+            Center = new Point(a.Center);
+            _height = a.Height;
+            Radius = a.Radius;
+        }
+
+        public double getVolume()
+        {
+            return getArena() * _height;
+        }
+        public double getSurface()
+        {
+            return 2 * getArena() + Math.PI * Radius * 2 * _height;
+        }
+        override public string ToString()
+        {
+            return "[Cylinder: center(" + Center.X + ", " + Center.Y + "), radius=" + Radius + ", height=" +_height + ", surface=" + getSurface() + ", volume=" + getVolume() +"]";
+        }
+    }
 }
 
--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -4,9 +4,50 @@ namespace Lab4
 {
 	public class Point
 	{
-		public Point ()
+        private double _x, _y;
+
+        public double X
+        {
+            get
+            {
+                return _x;
+            }
+
+            set
+            {
+                _x = value;
+            }
+        }
+        public double Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+
+        public Point ()
 		{
 		}
-	}
+        public Point (Point a)
+        {
+            X = a.X;
+            Y = a.Y;
+        }
+        public Point (double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
 }
 
