7766103ca22ed3d1ea642eaf0646c85eae70ec74
JANISTA SRILERJAN <janista_sr@cmu.ac.th>
2015-09-18 17:46:19 +0700

    570610555 lab401

--- week4/Lab401/Point.cs
+++ week4/Lab401/Point.cs
@@ -4,9 +4,56 @@ namespace Lab4
 {
 	public class Point
 	{
-		public Point ()
-		{
-		}
-	}
+		
+        private double _x;
+        private double _y;
+        public Point()
+        {
+
+            X = 0;
+            Y = 0;
+        }
+
+        public double X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+        public double Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+        public Point(Point a)
+        {
+            X = a._x;
+            Y = a._y;
+        }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+
+            return string.Format("{{0},{1}}", X, Y);
+        }
+    }
 }
+	
 
