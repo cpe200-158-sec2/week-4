5a4c103921f4ea96ea4ccb18f0adb5fa7a21df7d
mindmie <dreamiing.allday@gmail.com>
2015-09-23 17:06:03 +0700

    570610612 Lab402

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -4,9 +4,32 @@ namespace Lab4
 {
 	public abstract class Shape
 	{
-		public Shape ()
-		{
-		}
-	}
+        //member
+        public abstract double getArea();
+        public abstract double getPerimeter();
+
+        protected string _color ; //private string _color;
+
+        //properties
+        public string Color //read noly
+        {
+            get { return _color; }
+        }
+
+        //constructor default = "gold"
+        public Shape(string c = "gold")
+        {
+            _color = c;
+        }
+
+        //override
+        public override string ToString()
+        {
+            return string.Format("{0}", Color);
+        }
+
+
+
+    }
 }
 
--- week4/Lab402/Triangle.cs
+++ week4/Lab402/Triangle.cs
@@ -2,11 +2,86 @@
 
 namespace Lab4
 {
-	public class Triangle
-	{
-		public Triangle ()
+	public class Triangle : Shape
+	{   //member
+        private double _height;
+        private double _width;
+        
+        //properties
+
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if(value > 0)
+                {
+                    _height = value;
+                }
+                else
+                {
+                    _height = 1.0;
+                }
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                if( value > 0)
+                {
+                    _width = value;           
+                }
+                else
+                {
+                    _width = 1.0;
+                }
+            }
+        }
+        //constructor
+
+		public Triangle () 
 		{
+            _color = "gold"; //-
+            Height = 1.0;
+            Width = 1.0;
 		}
-	}
+        public Triangle( string c, double w ,double h) //: base(c)
+        {
+            _color = c; //-
+            Height = h;
+            Width = w;
+        }
+        public Triangle(Triangle a) //: base(a.Color)
+        {
+            _color = a.Color; //-
+            Height = a.Height;
+            Width = a.Width;
+        }
+
+        //method
+        public override double getArea()
+        {
+            return 0.5 * Width * Height;
+        }
+        public override double getPerimeter()
+        {
+            return Width + Height + Math.Sqrt((Width * Width) + (Height * Height));
+        }
+        public override string ToString()
+        {
+            return string.Format("[Triangle: base=" + Width + ", height=" + Height + ", area=" + getArea() + ", color=\""+ Color + "\"]");
+        }
+
+
+
+    }
 }
 
