789aa32a042f4db8e5c9d295c1cfe2ef39094c5a
JANISTA SRILERJAN <janista_sr@cmu.ac.th>
2015-09-24 19:21:04 +0700

    570610555 lab 402 ค่ะ^^

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -4,9 +4,30 @@ namespace Lab4
 {
 	public abstract class Shape
 	{
-		public Shape ()
+        private string _color;
+        public abstract double getArea();
+        public abstract double getPerimeter();
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+        }
+     
+		public Shape (string c="gold")
 		{
+            _color=c;
 		}
-	}
+        
+
+        public override string ToString()
+        {
+            string st;
+            st = "[Color:" + Color + "]";
+            return st;
+        }
+
+    }
 }
 
--- week4/Lab402/Triangle.cs
+++ week4/Lab402/Triangle.cs
@@ -2,11 +2,82 @@
 
 namespace Lab4
 {
-	public class Triangle
+	public class Triangle : Shape
 	{
-		public Triangle ()
+        private double _width;
+        private double _height;
+
+        public override double getArea()
+        {
+            return Width * Height * 0.5;
+        }
+        public override double getPerimeter()
+        {
+            return Width + Height + Math.Sqrt((Width * Width) + (Height * Height));
+        }
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _width = value;
+                }
+                else
+                {
+                    _width = 1.0;
+                }
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _height = value;
+                }
+                else
+                {
+                    _height = 1.0;
+                }
+            }
+        }
+        public Triangle ()
 		{
-		}
-	}
+            Height = 1.0;
+            Width = 1.0;
+            
+        }
+
+        public Triangle(string c,double w,double h):base(c)
+        {
+           
+            Height = h;
+            Width = w;
+        }
+
+        public Triangle(Rectangle r):base(r.Color)
+        {
+            Height = r.Height;
+            Width = r.Width;
+        }
+
+        public override string ToString()
+        {
+            string st;
+            st = "[Triangle: base=" + Width + " ,height=" + Height + " ,area=" + getArea() + " ,color= \"" + Color + "\"]";
+            return st;
+        }
+    }
 }
 
