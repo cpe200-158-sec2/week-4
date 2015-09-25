9253301aca349a567328753779715c287f78459d
FliipFlop <hanry5546@gmail.com>
2015-09-25 15:13:53 +0700

    570610569 Lab402

---
+++ Lab402/Lab402/Rectangle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Lab4
+{
+	public class Rectangle : Shape
+	{
+        private double _Width;
+        private double _Height;
+
+        public double Width
+        {
+            get
+            {
+                return _Width;
+            }
+
+            set
+            {
+                if(value > 0)
+                {
+                    _Width = value;
+                }
+                else
+                {
+                    _Width = 1;
+                }              
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return _Height;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    _Height = value;
+                }
+                else
+                {
+                    _Height = 1;
+                }
+            }
+        }
+
+        public Rectangle ()
+		{
+            Width = 1;
+            Height = 1;
+		}
+
+        public Rectangle (string colour , double w , double h)
+        {
+            Color = colour;
+            Width = w;
+            Height = h;
+        }
+
+        public Rectangle (Rectangle a)
+        {
+            Color = a.Color;
+            Width = a.Width;
+            Height = a.Height;
+        }
+
+        public override double getArea()
+        {
+            return Width * Height;
+        }
+
+        public override string ToString()
+        {
+            return "[Rectangle: width=" + Width + ", height=" + Height + ", area=" + getArea() + ", color=" + Color + "]";
+        }
+
+        public override double getPerimeter()
+        {
+            return Width * 2 + Height * 2;
+        }
+    }
+}
+
---
+++ Lab402/Lab402/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab4
+{
+	public abstract class Shape
+	{
+        private string _Color = "gold";
+        public string Color
+        {
+            get
+            {
+                return _Color;
+            }
+
+            set
+            {
+                _Color = value;
+            }
+        }
+
+        public Shape()
+		{
+
+		}
+
+        public Shape(string a)
+        {
+            Color = a;
+        }
+
+
+        
+        public abstract double getArea();
+        public abstract double getPerimeter();
+
+
+    }
+}
+
