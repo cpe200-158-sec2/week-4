590deb14cad1ee8ba8c7dfc92ed540d054048427
oommi03 <oommi04@hotmail.com>
2015-09-25 15:12:34 +0700

    570610579 lab 402

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace Lab4
+namespace Lab402
 {
-	public abstract class Shape
-	{
-		public Shape ()
-		{
-		}
-	}
+    public abstract class Shape
+    {
+        private string color;
+
+        public string Color
+        {
+            get { return color; }
+        }
+
+        public Shape(string c = "gold")
+        {
+            color = c;
+        }
+
+        public abstract double getArea();
+        public abstract double getPerimeter();
+
+        public override string ToString()
+        {
+            return Color;
+        }
+
+    }
 }
 
