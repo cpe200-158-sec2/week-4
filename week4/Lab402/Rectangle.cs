e1fd03f160313e888cafef9d5f730ace5d492dc3
pansitwattana <jjsqling@gmail.com>
2015-09-25 12:23:36 +0700

    570610588 Lab402 done

--- week4/Lab402/Rectangle.cs
+++ week4/Lab402/Rectangle.cs
@@ -2,11 +2,59 @@
 
 namespace Lab4
 {
-	public class Rectangle
-	{
-		public Rectangle ()
-		{
-		}
-	}
+    public class Rectangle : Shape
+    {
+
+
+        public Rectangle() : base("gold")
+        {
+            Width = 1;
+            Height = 1;
+        }
+
+        public Rectangle(string c, double w, double h) : base(c)
+        {
+            Width = w;
+            Height = h;
+        }
+
+        public Rectangle(Rectangle rec) : base(rec.Color)
+        {
+            Width = rec.Width;
+            Height = rec.Height;
+        }
+
+        public override double getArea()
+        {
+            return Width * Height;
+        }
+
+        public override double getPerimeter()
+        {
+            return Width + Width + Height + Height;
+        }
+
+        public override string ToString()
+        {
+            return "[Rectangle: width=" + Width + ", height=" + Height + ", area=" + getArea() + ", color=\"" + Color + "\"]";
+        }
+
+    }
 }
+//# 2. Rectangle Class:
+
+//## Properties:
+//- double Width, Height: dimension of rectangle(>0, default=1.0)
+
+//## Constructors:
+//- Rectangle(): default
+//- Rectangle(string, double, double): takes color, width, height as parameters
+//- Rectangle(Rectangle): copy constructor
+
+//## Methods:
+//- getArea(): get area of a rectangle
+//- override ToString() method [Rectangle: width=4, height=5, area=20, color="red"]
+
+
+
 
--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -2,11 +2,66 @@
 
 namespace Lab4
 {
-	public abstract class Shape
-	{
-		public Shape ()
-		{
-		}
-	}
+    public abstract class Shape
+    {
+        private string color = "gold";
+
+        private double width = 1.0;
+
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value >= 0)
+                    width = value;
+                else width = 1;
+            }
+        }
+
+        private double height = 1.0;
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value >= 0)
+                    height = value;
+                else height = 1;
+            }
+        }
+        public string Color
+        {
+            get { return color; }
+        }
+
+        public Shape(string color)
+        {
+            this.color = color;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public abstract double getArea();
+        public abstract double getPerimeter();
+    }
 }
+//# 1. Shape Class:
+//Abstract class
+
+//## Propeties:
+//- string Color: read-only, color of the shape, default="gold"
+
+//## Constructors:
+//- no default constructor
+//- Shape(string): takes color as a parameter
+
+//## Methods
+//- overriding ToString()
+//- getArea(): abstract method, calculate area of the shape
+//- getPerimeter(): abstract method, calculate perimeter of the shape
 
