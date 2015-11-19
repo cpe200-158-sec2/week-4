1cedc3f00ff6e5566511f7a9bc26d04cd7723c35
solomunji <class10_5@hotmail.co.th>
2015-11-19 22:50:23 +0700

    Update Shape.cs

--- week4/Lab402/Shape.cs
+++ week4/Lab402/Shape.cs
@@ -1,12 +1,28 @@
-﻿using System;
+using System;
 
 namespace Lab4
 {
-	public abstract class Shape
-	{
-		public Shape ()
-		{
-		}
-	}
-}
+    public abstract class Shape
+    {
+        private string _color;
 
+        public string Color
+        {
+            get { return _color; }
+        }
+
+        public Shape(string c = "gold")
+        {
+            _color = c;
+        }
+
+        public abstract double getArea();
+        public abstract double getPerimeter();
+
+        public override string ToString()
+        {
+            return Color;
+        }
+
+    }
+}
